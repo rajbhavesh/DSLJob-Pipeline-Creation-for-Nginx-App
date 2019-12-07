944b5e8f8b8f7430d4dc1818904723c5acacb6cb
rajbhavesh <58482189+rajbhavesh@users.noreply.github.com>
2019-12-07 17:53:43 +0530

    Update DSL.groovy

--- DSL.groovy
+++ DSL.groovy
@@ -1,5 +1,5 @@
 pipelineJob('Job-NginxAppCreation') {
-     description('This is the job for creating a single node kubernetes cluster.')
+     description('This is the job for creating a Pipeline Job for Simple Nginx based application Deployment.')
     definition {
         cpsScm {
             scm {
